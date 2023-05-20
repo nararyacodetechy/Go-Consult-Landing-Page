61e215a9b9890cc8dc68a1c15c7e7d642bf3f450
Bagus Nararya Nanda Raditya <87893990+nararyacodetechy@users.noreply.github.com>
2023-05-20 08:49:05 +0800

    Add Form Registration Consultant

--- src/scripts/views/pages/be-consult-page.js
+++ src/scripts/views/pages/be-consult-page.js
@@ -2,7 +2,31 @@ const BeConsultPage = {
   async render() {
     return `
           <h5 class="title-page">Connect to be Consultan</h5>
-          <section class="be-consultant"></section>
+          <section class="be-consultant">
+            <h2 class="main-title">Consultant Registration Form</h2>
+            <form>
+              <div class="registration-consultant">
+                <div class="required-input">
+                  <input type="text" placeholder="Full Name">
+                  <input type="email" placeholder="Email">
+                  <input type="tel" placeholder="Telephone">
+                  <input type="text" placeholder="Address">
+                  <input type="text" placeholder="Education">
+                  <input type="text" placeholder="Expertise">
+                </div>
+                <input class="upload-foto" type="image" src="./images/upload-foto.png">
+              </div>
+              <div class="description-consultant">
+                <textarea placeholder="Describe Yourself"></textarea>
+                <textarea placeholder="Your reason why you want to be consultant"></textarea>
+              </div>
+              <div class="save-login">
+                  <input type="checkbox" id="save">
+                  <label for="save">By signing this, it means you have followed the terms and conditions as a consultant</label>
+              </div>
+              <button type="submit">Submit Application</button>
+            </form>
+          </section>
         `;
   },
 
